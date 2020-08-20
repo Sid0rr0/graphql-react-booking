1208a6ce27b7efd3d5bb0cb7934855ee1d78e266
Sid0rr0 <elhungus@gmail.com>
2020-08-20 19:08:39 +0200

    Modal and better styling

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Events.css";
+import Modal from "../components/modal/Modal.js";
+import Backdrop from "../components/backdrop/Backdrop.js";
 
 export default function Events() {
+	const [creating, setCreating] = useState(false);
+
+	function modalCancelHandler() {
+		setCreating(false);
+	}
+
+	function modalConfirmHandler() {
+		setCreating(false);
+	}
+
 	return (
-		<div>
-			<h1>The Events Page</h1>
-		</div>
-	)
+		<>
+			{creating && <Backdrop />}
+			{creating && (
+				<Modal
+					title="Add Event"
+					canCancel
+					canConfirm
+					onCancel={modalCancelHandler}
+					onConfirm={modalConfirmHandler}
+				>
+					<p>Modal Content</p>
+				</Modal>
+			)}
+			<div className="events-control">
+				<p>Share your own Events!</p>
+				<button className="btn" onClick={() => setCreating(true)}>
+					Create Event
+				</button>
+			</div>
+		</>
+	);
 }
