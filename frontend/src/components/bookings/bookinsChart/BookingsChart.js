c3afcb8c950da8b1f98a3f72a9225cc72e4e59c8
Sid0rr0 <elhungus@gmail.com>
2020-09-12 15:11:02 +0200

    Added chart for booking

--- frontend/src/components/bookings/bookinsChart/BookingsChart.js
+++ frontend/src/components/bookings/bookinsChart/BookingsChart.js
@@ -1,5 +1,52 @@
 import React from "react";
+import { Bar } from "react-chartjs-2";
 
-export default function BookingsChart() {
-	return <div>chart</div>;
+export default function BookingsChart(props) {
+	const BOOKINGS_BUCKETS = {
+		Cheap: {
+			min: 0,
+			max: 50,
+		},
+		Normal: {
+			min: 50,
+			max: 100,
+		},
+		Expensive: {
+			min: 100,
+			max: 1000000,
+		},
+	};
+
+	const chartData = { labels: [], datasets: [] };
+	let values = [];
+	for (const bucket in BOOKINGS_BUCKETS) {
+		const filteredBookingsCount = props.bookings.reduce((prev, curr) => {
+			if (
+				curr.event.price > BOOKINGS_BUCKETS[bucket].min &&
+				curr.event.price < BOOKINGS_BUCKETS[bucket].max
+			) {
+				return prev + 1;
+			}
+			return prev;
+		}, 0);
+		values.push(filteredBookingsCount);
+		chartData.labels.push(bucket);
+		chartData.datasets.push({
+			label: bucket,
+			backgroundColor: "rgba(255,99,132,0.2)",
+			borderColor: "rgba(255,99,132,1)",
+			borderWidth: 1,
+			hoverBackgroundColor: "rgba(255,99,132,0.4)",
+			hoverBorderColor: "rgba(255,99,132,1)",
+			data: values,
+		});
+		values = [...values];
+		values[values.length - 1] = 0;
+	}
+
+	return (
+		<div style={{ textAlign: "center" }}>
+			<Bar data={chartData} />
+		</div>
+	);
 }
--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -65,6 +65,7 @@ export default function Bookings() {
 							_id
 							title
 							date
+							price
 						}
 					}
 				}
